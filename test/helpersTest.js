2f7a9c3a2fe6f51c041c7ff69eff827974ec3a8b
edgmsh <edgmsh@gmail.com>
2024-10-31 12:45:45 -0400

    Testing Helper Functions

---
+++ test/helpersTest.js
@@ -0,0 +1,44 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "[email protected1]", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "[email protected2]", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail(testUsers,"[email protected1]")
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID);
+  });
+  it('should not return a user with valid email', function() {
+    const user = getUserByEmail(testUsers,"[email protected3]")
+    const expectedUser = false;
+    assert.equal(user, expectedUser);
+  });
+  it('should not return a user with valid email', function() {
+    const user = getUserByEmail(testUsers,"")
+    const expectedUser = false;
+    assert.equal(user, expectedUser);
+  });
+  it('should not return a user with valid email', function() {
+    const user = getUserByEmail(testUsers,undefined)
+    const expectedUser = false;
+    assert.equal(user, expectedUser);
+  });
+  it('should not return a user with valid email', function() {
+    const user = getUserByEmail(testUsers,null)
+    const expectedUser = false;
+    assert.equal(user, expectedUser);
+  });
+});
